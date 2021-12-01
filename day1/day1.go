4a4560b83da533532e653ff0a6802ade1e230ce8
Kamaal <kamaal@tranzer.com>
2021-12-02 00:25:31 +0100

    Solving part 2 of day 1

--- day1/day1.go
+++ day1/day1.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"strconv"
@@ -9,18 +8,61 @@ import (
 )
 
 func main() {
-	inputFileBytes, err := ioutil.ReadFile("./input.txt")
+	log.Println("Part 1:", part1())
+	log.Println("Part 2:", part2())
+}
+
+func part2() int {
+	lines, err := getInputLines()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var increases = 0
-	lines := strings.Split(string(inputFileBytes), "\n")
+	increases := 0
 	for lineNumber, line := range lines {
 		intLine, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		if lineNumber+3 < len(lines) {
+			firstSum := intLine
+			secondSum := 0
+			for i := 0; i < 3; i += 1 {
+				nextLine, err := strconv.Atoi(lines[lineNumber+i+1])
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if i < 2 {
+					firstSum = firstSum + nextLine
+				}
+
+				secondSum = secondSum + nextLine
+			}
+
+			if secondSum > firstSum {
+				increases += 1
+			}
+		}
+	}
+
+	return increases
+}
+
+func part1() int {
+	lines, err := getInputLines()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	increases := 0
+	for lineNumber, line := range lines {
+		intLine, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		if lineNumber+1 < len(lines) {
 			nextLine, err := strconv.Atoi(lines[lineNumber+1])
 			if err != nil {
@@ -28,10 +70,19 @@ func main() {
 			}
 
 			if intLine < nextLine {
-				increases++
+				increases += 1
 			}
 		}
 	}
 
-	fmt.Println(increases)
+	return increases
+}
+
+func getInputLines() ([]string, error) {
+	inputFileBytes, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(inputFileBytes), "\n")
+	return lines, nil
 }
