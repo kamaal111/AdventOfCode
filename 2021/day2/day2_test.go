10bdc27b879ac7e6fce142fa054959dae6dd6507
kamaal111 <kamaal.f1@gmail.com>
2021-12-03 21:33:17 +0100

    Testing example of day 2

--- 2021/day2/day2_test.go
+++ 2021/day2/day2_test.go
@@ -8,6 +8,13 @@ import (
 
 const inputPath = "input.txt"
 
+const exampleInput = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`
+
 func TestPart1(t *testing.T) {
 	input, err := utils.GetInputString(inputPath)
 	if err != nil {
@@ -18,7 +25,16 @@ func TestPart1(t *testing.T) {
 	want := 2272262
 
 	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestPart1Example(t *testing.T) {
+	got := Part1(exampleInput)
+	want := 150
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
 
@@ -30,6 +46,15 @@ func TestPart2(t *testing.T) {
 
 	got := Part2(input)
 	want := 2134882034
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestPart2Example(t *testing.T) {
+	got := Part2(exampleInput)
+	want := 900
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
