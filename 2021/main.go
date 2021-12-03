78cfc2199b468664ad551adfc790bca558a05447
Kamaal <kamaal@tranzer.com>
2021-12-03 17:28:36 +0100

    Commenting results

--- 2021/main.go
+++ 2021/main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	log.Println("Day 1 Part 1:", day1.Part1())
-	log.Println("Day 1 Part 2:", day1.Part2())
+	log.Println("Day 1 Part 1:", day1.Part1()) // 1374
+	log.Println("Day 1 Part 2:", day1.Part2()) // 1418
 
-	log.Println("Day 2 Part 1:", day2.Part1())
-	log.Println("Day 2 Part 2:", day2.Part2())
+	log.Println("Day 2 Part 1:", day2.Part1()) // 2272262
+	log.Println("Day 2 Part 2:", day2.Part2()) // 2134882034
 
-	log.Println("Day 3 Part 1:", day3.Part1())
-	log.Println("Day 3 Part 2:", day3.Part2())
+	log.Println("Day 3 Part 1:", day3.Part1()) // 1025636
+	log.Println("Day 3 Part 2:", day3.Part2()) // 793873
 }
