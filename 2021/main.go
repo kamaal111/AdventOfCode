af05fd765e8d89a13f1219a5a28a229505ffda71
kamaal111 <kamaal.f1@gmail.com>
2021-12-03 19:20:26 +0100

    Timing functions

--- 2021/main.go
+++ 2021/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/kamaal111/AdventOfCode/2021/day1"
 	"github.com/kamaal111/AdventOfCode/2021/day2"
@@ -13,6 +14,8 @@ const inputPathDay2 = "day2/input.txt"
 const inputPathDay3 = "day3/input.txt"
 
 func main() {
+	start := time.Now()
+
 	log.Println("Day 1 Part 1:", day1.Part1(inputPathDay1))
 	log.Println("Day 1 Part 2:", day1.Part2(inputPathDay1))
 
@@ -21,4 +24,7 @@ func main() {
 
 	log.Println("Day 3 Part 1:", day3.Part1(inputPathDay3))
 	log.Println("Day 3 Part 2:", day3.Part2(inputPathDay3))
+
+	elapsed := time.Since(start)
+	log.Printf("Time: %s", elapsed)
 }
