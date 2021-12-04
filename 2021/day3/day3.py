247ed503425efc9fb4571c9feb9cebc94a85aec3
kamaal111 <kamaal.f1@gmail.com>
2021-12-04 18:33:35 +0100

    Solving day 3 part 2 in python

--- 2021/day3/day3.py
+++ 2021/day3/day3.py
@@ -1,13 +1,80 @@
-# day2.py
+from typing import List
 
 
 def run(input: str):
     print(f"Day 3 Part 1: {part1(input)}")
-    print(f"Day 3 Part 2: {part2(input)}")
+    print(f"Day 3 Part 2: {part2(input)}")  # 793873
 
 
 def part2(input: str):
-    return 0
+    lines = input.splitlines()
+
+    oxygen = get_oxygen(lines)
+    scrubber = get_scrubber(lines)
+
+    return int(oxygen, 2) * int(scrubber, 2)
+
+
+def get_scrubber(lines: List[str]):
+    bit_criteria = lines
+
+    length_of_first_line = len(lines[0])
+    for i in range(length_of_first_line):
+        zeros = 0
+        zeros_bitcriteria: List[str] = []
+        ones = 0
+        ones_bitcriteria: List[str] = []
+
+        for criteria in bit_criteria:
+            if criteria[i] == "0":
+                zeros += 1
+                zeros_bitcriteria.append(criteria)
+            else:
+                ones += 1
+                ones_bitcriteria.append(criteria)
+
+        if zeros <= ones:
+            bit_criteria = zeros_bitcriteria
+        elif ones < zeros:
+            bit_criteria = ones_bitcriteria
+
+        if len(bit_criteria) <= 1:
+            return bit_criteria[0]
+
+    return ""
+
+
+def get_oxygen(lines: List[str]):
+    bit_criteria = lines
+
+    oxygen = ""
+
+    length_of_first_line = len(lines[0])
+    for i in range(length_of_first_line):
+        zeros = 0
+        zeros_bitcriteria: List[str] = []
+        ones = 0
+        ones_bitcriteria: List[str] = []
+
+        for criteria in bit_criteria:
+            if criteria[i] == "0":
+                zeros += 1
+                zeros_bitcriteria.append(criteria)
+            else:
+                ones += 1
+                ones_bitcriteria.append(criteria)
+
+        if zeros > ones:
+            oxygen += "0"
+            bit_criteria = zeros_bitcriteria
+        elif ones >= zeros:
+            oxygen += "1"
+            bit_criteria = ones_bitcriteria
+
+        if len(bit_criteria) <= 1:
+            return bit_criteria[0]
+
+    return oxygen
 
 
 def part1(input: str):
