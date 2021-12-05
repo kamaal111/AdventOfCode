26725b40cc40a0015621d048df33bbd74d1953c8
kamaal111 <kamaal.f1@gmail.com>
2021-12-05 10:36:09 +0100

    Solving day 5 part 1 in python

--- 2021/day5/day5.py
+++ 2021/day5/day5.py
@@ -1,3 +1,23 @@
+from typing import List
+
+
+example_input = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"""
+
+
+STARTING_PLOT = "."
+FIRST_PLOT = "1"
+SECOND_PLOT = "2"
+
+
 def run(input: str):
     print(f"Day 5 Part 1: {part1(input)}")
     print(f"Day 5 Part 2: {part2(input)}")
@@ -8,4 +28,86 @@ def part2(input: str):
 
 
 def part1(input: str):
-    return 0
+    lines = input.splitlines()
+
+    grid_size = 0
+    for line in lines:
+        for item in line.split("->"):
+            for coordinate in make_coordinates(item):
+                grid_size = max(grid_size, coordinate)
+
+    grid_size += 1
+
+    plots: List[str] = []
+    for _ in range(grid_size * grid_size):
+        plots.append(STARTING_PLOT)
+
+
+    for line in lines:
+        coordinates = line.split("->")
+        first_coordinate = make_coordinates(coordinates[0])
+        second_coordinate = make_coordinates(coordinates[1])
+
+        if first_coordinate[0] == second_coordinate[0]: # vertical
+            if first_coordinate[1] > second_coordinate[1]:
+                minimum = second_coordinate[1]
+                maximum = first_coordinate[1]
+            else:
+                minimum = first_coordinate[1]
+                maximum = second_coordinate[1]
+
+            for i in range(minimum, maximum + 1):
+                plot_index = (i * grid_size + first_coordinate[0]) + 1
+
+                if plots[plot_index] == STARTING_PLOT:
+                    plots[plot_index] = FIRST_PLOT
+                elif plots[plot_index] == FIRST_PLOT:
+                    plots[plot_index] = SECOND_PLOT
+            
+        elif first_coordinate[1] == second_coordinate[1]: # horizontal
+            if first_coordinate[0] > second_coordinate[0]:
+                minimum = second_coordinate[0]
+                maximum = first_coordinate[0]
+            else:
+                minimum = first_coordinate[0]
+                maximum = second_coordinate[0]
+
+            for i in range(minimum, maximum + 1):
+                plot_index = (first_coordinate[1] * grid_size + i) + 1
+
+                if plots[plot_index] == STARTING_PLOT:
+                    plots[plot_index] = FIRST_PLOT
+                elif plots[plot_index] == FIRST_PLOT:
+                    plots[plot_index] = SECOND_PLOT
+
+
+    too_many_overlaps = 0
+    ordered_plots: List[List[str]] = []
+    for plot in plots:
+        if len(ordered_plots) == 0:
+            ordered_plots.append([plot])
+        
+        last_ordered_plot = ordered_plots[-1]
+        if len(last_ordered_plot) < grid_size:
+            last_ordered_plot.append(plot)
+            ordered_plots[-1] = last_ordered_plot
+        else:
+            ordered_plots.append([plot])
+
+        if plot == SECOND_PLOT:
+            too_many_overlaps += 1
+
+    # for ordered_plot in ordered_plots:
+    #     print(ordered_plot)
+
+    return too_many_overlaps
+
+
+def make_coordinates(coordinates: str):
+    coordinate: List[int] = []
+    for number in coordinates.split(","):
+        coordinate.append(int(number))
+    return coordinate
+
+
+# part1(example_input)
