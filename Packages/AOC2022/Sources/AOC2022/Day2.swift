6a0b358850765851d50fbd4434ca57c327fc5fce
Kamaal <kamaal.f1@gmail.com>
2022-12-02 07:36:49 +0100

    Solving day2

--- Packages/AOC2022/Sources/AOC2022/Day2.swift
+++ Packages/AOC2022/Sources/AOC2022/Day2.swift
@@ -8,6 +8,12 @@
 import Foundation
 import ShrimpExtensions
 
+private let OPPONENT_STRATEGY: [String: ShapePlay] = [
+    "A": .rock,
+    "B": .paper,
+    "C": .scissors,
+]
+
 extension AOC2022 {
     public struct Day2: DayScaffold {
         public static let resourceName = "day2"
@@ -18,7 +24,22 @@ extension AOC2022 {
             private init() { }
 
             public static func excecute(with input: String) -> Int {
-                0
+                let myStrategy: [String: ShapePlay] = [
+                    "X": .rock,
+                    "Y": .paper,
+                    "Z": .scissors,
+                ]
+
+                var myTotalScore = 0
+                for line in input.splitLines {
+                    let inputs = line.split(separator: " ").map({ String($0) })
+                    let opponentInput = OPPONENT_STRATEGY[inputs[0]]!
+                    let myInput = myStrategy[inputs[1]]!
+
+                    myTotalScore += myInput.scoreAgainstOpponent(opponentInput)
+                }
+
+                return myTotalScore
             }
         }
 
@@ -26,8 +47,107 @@ extension AOC2022 {
             private init() { }
 
             public static func excecute(with input: String) -> Int {
-                0
+                let myStrategy: [String: PlayDecision] = [
+                    "X": .lose,
+                    "Y": .draw,
+                    "Z": .win,
+                ]
+
+                var myTotalScore = 0
+                for line in input.splitLines {
+                    let inputs = line.split(separator: " ").map({ String($0) })
+                    let opponentInput = OPPONENT_STRATEGY[inputs[0]]!
+                    let myInput = myStrategy[inputs[1]]!.shapeAgainsOpponent(opponentInput)
+
+                    myTotalScore += myInput.scoreAgainstOpponent(opponentInput)
+                }
+
+                return myTotalScore
             }
         }
+    }
+}
+
+private enum PlayDecision: Int {
+    case win = 6
+    case lose = 0
+    case draw = 3
+
+    var score: Int {
+        rawValue
+    }
+
+    func shapeAgainsOpponent(_ opponent: ShapePlay) -> ShapePlay {
+        if self == .win {
+            switch opponent {
+            case .rock:
+                return .paper
+            case .paper:
+                return .scissors
+            case .scissors:
+                return .rock
+            }
+        }
+
+        if self == .lose {
+            switch opponent {
+            case .rock:
+                return .scissors
+            case .paper:
+                return .rock
+            case .scissors:
+                return .paper
+            }
+        }
+
+        return opponent
+    }
+}
+
+private enum ShapePlay: Int {
+    case rock = 1
+    case paper = 2
+    case scissors = 3
+
+    var score: Int {
+        rawValue
+    }
+
+    func scoreAgainstOpponent(_ opponent: ShapePlay) -> Int {
+        let ownScore = score
+
+        let decision: PlayDecision
+        if self == opponent {
+            decision = .draw
+        } else if self == .rock {
+            switch opponent {
+            case .scissors:
+                decision = .win
+            case .paper:
+                decision = .lose
+            default:
+                fatalError("should have been handled")
+            }
+        } else if self == .paper {
+            switch opponent {
+            case .rock:
+                decision = .win
+            case .scissors:
+                decision = .lose
+            default:
+                fatalError("should have been handled")
+            }
+        } else {
+            switch opponent { // scissors
+            case .rock:
+                decision = .lose
+            case .paper:
+                decision = .win
+            default:
+                fatalError("should have been handled")
+            }
+        }
+
+        return ownScore + decision.score
     }
 }
--- Packages/AOC2022/Tests/AOC2022Tests/AOC2022Day2Tests.swift
+++ Packages/AOC2022/Tests/AOC2022Tests/AOC2022Day2Tests.swift
@@ -10,26 +10,29 @@ import XCTest
 
 final class AOC2022Day2Tests: XCTestCase {
     let exampleInput = """
+A Y
+B X
+C Z
 """
 
     func testPart1() {
         let result = AOC2022.Day2.Part1.excecute(with: AOC2022.Day2.input)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 14375)
     }
 
     func testPart1Example() {
         let result = AOC2022.Day2.Part1.excecute(with: exampleInput)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 15)
     }
 
     func testPart2() {
         let result = AOC2022.Day2.Part2.excecute(with: AOC2022.Day2.input)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 10274)
     }
 
     func testPart2Example() {
         let result = AOC2022.Day2.Part2.excecute(with: exampleInput)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 12)
     }
 }
 
