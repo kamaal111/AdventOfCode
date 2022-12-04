d6683239c79fc49a816b0531332e81154a221dd2
Kamaal <kamaal.f1@gmail.com>
2022-12-04 06:51:02 +0100

    Solving day 4

--- Swift/AOC2022/Sources/AOC2022/Day4.swift
+++ Swift/AOC2022/Sources/AOC2022/Day4.swift
@@ -1,6 +1,6 @@
 //
 //  Day4.swift
-//  
+//
 //
 //  Created by Kamaal M Farah on 03/12/2022.
 //
@@ -14,14 +14,46 @@ extension AOC2022 {
 
         public enum Part1 {
             public static func execute(with input: String) -> Int {
-                return 0
+                var foundFullyOverlappingSchedules = 0
+                for schedulePairs in input.splitLines {
+                    let (firstSchedule, secondSchedule) = parseSchedulePairs(schedulePairs)
+                    if firstSchedule.contains(secondSchedule.lowerBound) &&
+                        firstSchedule.contains(secondSchedule.upperBound) {
+                        foundFullyOverlappingSchedules += 1
+                    } else if secondSchedule.contains(firstSchedule.lowerBound) &&
+                                secondSchedule.contains(firstSchedule.upperBound) {
+                        foundFullyOverlappingSchedules += 1
+                    }
+                }
+
+                return foundFullyOverlappingSchedules
             }
         }
 
         public enum Part2 {
             public static func execute(with input: String) -> Int {
-                return 0
+                var foundFullyOverlappingSchedules = 0
+                for schedulePairs in input.splitLines {
+                    let (firstSchedule, secondSchedule) = parseSchedulePairs(schedulePairs)
+                    if firstSchedule.contains(secondSchedule.lowerBound) ||
+                        firstSchedule.contains(secondSchedule.upperBound) {
+                        foundFullyOverlappingSchedules += 1
+                    } else if secondSchedule.contains(firstSchedule.lowerBound) ||
+                                secondSchedule.contains(firstSchedule.upperBound) {
+                        foundFullyOverlappingSchedules += 1
+                    }
+                }
+
+                return foundFullyOverlappingSchedules
             }
         }
     }
+}
+
+private func parseSchedulePairs(_ rawSchedulePairs: String.SubSequence) -> (ClosedRange<Int>, ClosedRange<Int>) {
+    let schedules = rawSchedulePairs.split(separator: ",")
+    let firstSchedule = schedules[0].split(separator: "-").map({ Int($0)! })
+    let secondSchedule = schedules[1].split(separator: "-").map({ Int($0)! })
+
+    return (firstSchedule[0]...firstSchedule[1], secondSchedule[0]...secondSchedule[1])
 }
--- Swift/AOC2022/Tests/AOC2022Tests/AOC2022Day4Tests.swift
+++ Swift/AOC2022/Tests/AOC2022Tests/AOC2022Day4Tests.swift
@@ -1,6 +1,6 @@
 //
 //  AOC2022Day4Tests.swift
-//  
+//
 //
 //  Created by Kamaal M Farah on 03/12/2022.
 //
@@ -10,27 +10,33 @@ import XCTest
 
 final class AOC2022Day4Tests: XCTestCase {
     let exampleInput = """
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
 """
 
     let input = AOC2022.Day4.input
 
     func testPart1() {
         let result = AOC2022.Day4.Part1.execute(with: input)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 560)
     }
 
     func testPart1Example() {
         let result = AOC2022.Day4.Part1.execute(with: exampleInput)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 2)
     }
 
     func testPart2() {
         let result = AOC2022.Day4.Part2.execute(with: input)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 839)
     }
 
     func testPart2Example() {
         let result = AOC2022.Day4.Part2.execute(with: exampleInput)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 4)
     }
 }
