8c7c980079aed5b74384b71b9bd0c5e404c38259
Kamaal <kamaal.f1@gmail.com>
2022-12-04 07:41:12 +0100

    Preparing day5

---
+++ Swift/AOC2022/Sources/AOC2022/Day5.swift
@@ -0,0 +1,27 @@
+//
+//  Day5.swift
+//
+//
+//  Created by Kamaal M Farah on 04/12/2022.
+//
+
+import Foundation
+import ShrimpExtensions
+
+extension AOC2022 {
+    public enum Day5: DayScaffold {
+        public static let resourceName = "day5"
+
+        public enum Part1 {
+            public static func execute(with input: String) -> Int {
+                0
+            }
+        }
+
+        public enum Part2 {
+            public static func execute(with input: String) -> Int {
+                0
+            }
+        }
+    }
+}
