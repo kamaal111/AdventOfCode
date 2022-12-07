7e965a51b5c345c24bffd6d36b4f206553b664ef
Kamaal <kamaal.f1@gmail.com>
2022-12-07 21:28:09 +0100

    Preparing day8

---
+++ Swift/AOC2022/Sources/AOC2022/Day8.swift
@@ -0,0 +1,27 @@
+//
+//  Day8.swift
+//
+//
+//  Created by Kamaal M Farah on 07/12/2022.
+//
+
+import Foundation
+import ShrimpExtensions
+
+extension AOC2022 {
+    public enum Day8: DayScaffold {
+        public static let resourceName = "day8"
+
+        public enum Part1 {
+            public static func execute(with input: String) -> Int {
+                0
+            }
+        }
+
+        public enum Part2 {
+            public static func execute(with input: String) -> Int {
+                0
+            }
+        }
+    }
+}
---
+++ Swift/AOC2022/Tests/AOC2022Tests/AOC2022Day8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AOC2022Day8Tests.swift
+//
+//
+//  Created by Kamaal M Farah on 07/12/2022.
+//
+
+import XCTest
+@testable import AOC2022
+
+final class AOC2022Day8Tests: XCTestCase {
+    let exampleInput = """
+"""
+
+    let input = AOC2022.Day8.input
+
+    func testPart1() {
+        let result = AOC2022.Day8.Part1.execute(with: input)
+        XCTAssertEqual(result, 0)
+    }
+
+    func testPart1Example() {
+        let result = AOC2022.Day8.Part1.execute(with: exampleInput)
+        XCTAssertEqual(result, 0)
+    }
+
+    func testPart2() {
+        let result = AOC2022.Day8.Part2.execute(with: input)
+        XCTAssertEqual(result, 0)
+    }
+
+    func testPart2Example() {
+        let result = AOC2022.Day8.Part2.execute(with: exampleInput)
+        XCTAssertEqual(result, 0)
+    }
+}
