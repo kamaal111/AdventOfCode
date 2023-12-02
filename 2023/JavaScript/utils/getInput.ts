adb5099d91f68b9b1d442f82dafe155ada190985
Kamaal Farah <kamaal.f1@gmail.com>
2023-12-02 11:25:27 +0100

    Solving day 2 in Rust

--- 2023/JavaScript/utils/getInput.ts
+++ 2023/JavaScript/utils/getInput.ts
@@ -6,7 +6,7 @@ export async function getInput(day: string): Promise<string> {
   const cachedInputs = _inputs[day];
   if (cachedInputs != null) return cachedInputs;
 
-  const input = await fs.readFile(`inputs/${day}.txt`, "utf-8");
+  const input = await fs.readFile(`../Inputs/${day}.txt`, "utf-8");
   const inputString = input.toString();
   _inputs[day] = inputString;
 
