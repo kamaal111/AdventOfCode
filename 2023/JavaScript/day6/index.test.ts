ab9ba4dd1096a231f421d9d4ab38c8eb77f2758f
Kamaal Farah <kamaal.f1@gmail.com>
2023-12-06 06:48:46 +0100

    Solving day6

--- 2023/JavaScript/day6/index.test.ts
+++ 2023/JavaScript/day6/index.test.ts
@@ -3,25 +3,26 @@ import { test, expect, describe } from "bun:test";
 import { part1, part2 } from ".";
 import { getInput } from "../utils/getInput";
 
-const EXAMPLE_INPUT = ``;
+const EXAMPLE_INPUT = `Time:      7  15   30
+Distance:  9  40  200`;
 
 describe("day6", () => {
   test("part1", async () => {
     const input = await getInput("day6");
-    expect(part1(input)).toBe(0);
+    expect(part1(input)).toBe(114400);
   });
 
   test("part1 with example input", () => {
-    expect(part1(EXAMPLE_INPUT)).toBe(0);
+    expect(part1(EXAMPLE_INPUT)).toBe(288);
   });
 
-  test("part2", async () => {
+  test.skip("part2", async () => {
     const input = await getInput("day6");
     const result = part2(input);
-    expect(result).toBe(0);
+    expect(result).toBe(21039729);
   });
 
   test("part2 with example input", () => {
-    expect(part2(EXAMPLE_INPUT)).toBe(0);
+    expect(part2(EXAMPLE_INPUT)).toBe(71503);
   });
 });
--- 2023/JavaScript/day6/index.ts
+++ 2023/JavaScript/day6/index.ts
@@ -1,7 +1,65 @@
+import isNumber from "../utils/isNumber";
+
 export function part1(input: string) {
-  return 0;
+  const { times, distances } = parseInput(input);
+  return times
+    .map((time, index) => calculateWaysToWin(time, distances[index]))
+    .reduce((acc, value) => acc * value, 1);
 }
 
+// Brute force in 48 seconds
 export function part2(input: string) {
-  return 0;
+  const { times, distances } = parseInput(input);
+  const time = Number(times.join(""));
+  const distance = Number(distances.join(""));
+  return calculateWaysToWin(time, distance);
+}
+
+function calculateWaysToWin(time: number, distance: number) {
+  const minimumHoldDownTimeToWin = getMinimumHoldDownTimeToWin(time, distance);
+  const maximumHoldDownTimeToWin = getMaximumHoldDownTimeToWin(
+    minimumHoldDownTimeToWin,
+    time,
+    distance,
+  );
+  return maximumHoldDownTimeToWin - minimumHoldDownTimeToWin + 1;
+}
+
+function getMaximumHoldDownTimeToWin(
+  minimumTime: number,
+  time: number,
+  distance: number,
+) {
+  for (let index = time; index > minimumTime; index -= 1) {
+    const distanceTraveled = calculateDistanceTraveled(time, index);
+    console.log(`going from ${index} to ${minimumTime}`);
+    if (distanceTraveled > distance) return index;
+  }
+
+  throw new Error("No maximum found");
+}
+
+function getMinimumHoldDownTimeToWin(time: number, distance: number) {
+  for (let index = 1; index < time; index += 1) {
+    const distanceTraveled = calculateDistanceTraveled(time, index);
+    console.log(`going from ${index} to ${time}`);
+    if (distanceTraveled > distance) return index;
+  }
+
+  throw new Error("No minimum found");
+}
+
+function calculateDistanceTraveled(totalTime: number, holdDown: number) {
+  return holdDown * (totalTime - holdDown);
+}
+
+function parseInput(input: string) {
+  const [times, distances] = input.split("\n").map((line) =>
+    line
+      .split(":")[1]
+      .split(" ")
+      .filter((value) => isNumber(value))
+      .map((value) => Number(value)),
+  );
+  return { times, distances };
 }
