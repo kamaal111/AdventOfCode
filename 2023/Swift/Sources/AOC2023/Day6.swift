71515a8f6bd63863019a17a50377a8bf0f5894f3
Kamaal Farah <kamaal.f1@gmail.com>
2023-12-08 19:52:50 +0100

    Solving day6 in Swift

---
+++ 2023/Swift/Sources/AOC2023/Day6.swift
@@ -0,0 +1,48 @@
+//
+//  Day6.swift
+//
+//
+//  Created by Kamaal M Farah on 08/12/2023.
+//
+
+import AOCTools
+import Foundation
+import KamaalExtensions
+
+enum Day6: DayScaffold {
+    static var resourceName = "day6"
+
+    static func part1(input: String) -> Int {
+        let (times, distances) = parseInput(input)
+        return zip(times, distances)
+            .map({ calculateWaysToWin(time: Double($0), distance: Double($1)) })
+            .reduce(1, { $0 * $1 })
+            .int
+    }
+
+    static func part2(input: String) -> Int {
+        let (times, distances) = parseInput(input)
+        let time = Double(times.map({ String($0) }).joined())!
+        let distance = Double(distances.map({ String($0) }).joined())!
+        return calculateWaysToWin(time: time, distance: distance).int
+    }
+}
+
+private func calculateWaysToWin(time: Double, distance: Double) -> Double {
+    let (shortest, longest) = Maths.quadraticEquation(time, distance, 1)
+    return longest - shortest + 1
+}
+
+private func parseInput(_ input: String) -> (times: [Int], distances: [Int]) {
+    let items = input
+        .splitLines
+        .compactMap({
+            $0
+                .split(separator: ":")
+                .last?
+                .split(separator: " ")
+                .compactMap({ $0.int })
+        })
+    assert(items.count == 2)
+    return (items[0], items[1])
+}
