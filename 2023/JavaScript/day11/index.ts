97dbada9e911aedf6e5b312143bd64a82b1c0a53
Kamaal Farah <kfarah@box.com>
2023-12-11 18:53:16 +0100

    WIP day10 and day11

--- 2023/JavaScript/day11/index.ts
+++ 2023/JavaScript/day11/index.ts
@@ -1,25 +1,42 @@
-import Matrix from "../models/matrix";
+import Matrix, { type MatrixElement } from "../models/matrix";
+import { sum } from "../utils/maths";
 
 type Cosmos = "." | "#";
 
+interface GalaxyPair {
+  first: MatrixElement<Cosmos>;
+  last: MatrixElement<Cosmos>;
+}
+
 export function part1(input: string) {
   const cosmos = parseInput(input);
-  makePairs(cosmos);
-
-  return 0;
+  return sum(makePairs(cosmos).map(calculateLengthBetweenGalaxies));
 }
 
 export function part2(input: string) {
   return 0;
 }
 
-function makePairs(cosmos: Matrix<Cosmos>) {
+function calculateLengthBetweenGalaxies(pair: GalaxyPair) {
+  return 0;
+}
+
+function makePairs(cosmos: Matrix<Cosmos>): GalaxyPair[] {
   const galaxies = cosmos.elements.flat().filter((item) => item.value === "#");
-  for (let index = 0; index < galaxies.length; index += 1) {
-    const galaxy = galaxies[index];
-    console.log("galaxy", galaxy);
+  const pairs = new Set<string>();
+  for (let x = 0; x < galaxies.length - 1; x += 1) {
+    for (let y = 1; y < galaxies.length; y += 1) {
+      if (x === y) continue;
+
+      const first = Math.min(x, y);
+      const last = Math.max(x, y);
+      pairs.add(`${first}-${last}`);
+    }
   }
-  // const pairs: Array<MatrixElement<Cosmos>> = [];
+  return [...pairs].map((pairsKey) => {
+    const [first, last] = pairsKey.split("-").map((index) => Number(index));
+    return { first: galaxies[first], last: galaxies[last] };
+  });
 }
 
 function parseInput(input: string) {
