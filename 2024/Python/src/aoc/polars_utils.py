30470715ffae6eb5b856c5b55daac8afdf762613
Kamaal Farah <kamaal.f1@gmail.com>
2024-12-07 12:52:50 +0100

    Upgrading python

--- 2024/Python/src/aoc/polars_utils.py
+++ 2024/Python/src/aoc/polars_utils.py
@@ -1,11 +1,11 @@
 from typing import Callable
 
 import polars as pl
-from polars.dataframe.group_by import GroupBy
+from polars.lazyframe.group_by import LazyGroupBy
 
 
 def pl_group_all(
-    data_frame: pl.LazyFrame, aggregations: Callable[[GroupBy], pl.LazyFrame]
+    data_frame: pl.LazyFrame, aggregations: Callable[[LazyGroupBy], pl.LazyFrame]
 ) -> pl.LazyFrame:
     return aggregations(
         data_frame.with_columns(pl.lit("🐸").alias("🐸")).group_by("🐸")
