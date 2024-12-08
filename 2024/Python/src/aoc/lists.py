c8d574f94726c5909e3a9e722f89d8cfceeb1241
Kamaal Farah <kamaal.f1@gmail.com>
2024-12-08 11:11:45 +0100

    Bit of day8 refactoring

--- 2024/Python/src/aoc/lists.py
+++ 2024/Python/src/aoc/lists.py
@@ -1,10 +1,28 @@
 from functools import cmp_to_key
-from typing import Callable, TypeVar
+from typing import Callable, Iterable, TypeVar
 
 ElementToSort = TypeVar("ElementToSort")
 
 
 def sorted_with_comparator(
-    data: list[ElementToSort], comparator: Callable[[ElementToSort, ElementToSort], int]
-):
-    return sorted(data, key=cmp_to_key(comparator))
+    items: Iterable[ElementToSort],
+    comparator: Callable[[ElementToSort, ElementToSort], int],
+) -> list[ElementToSort]:
+    return sorted(items, key=cmp_to_key(comparator))
+
+
+CompactedElement = TypeVar("CompactedElement")
+ElementToCompact = TypeVar("ElementToCompact")
+
+
+def compact_map(
+    items: Iterable[ElementToCompact | None],
+    predicate: Callable[[ElementToCompact | None], CompactedElement | None],
+) -> list[CompactedElement]:
+    compacted_items: list[CompactedElement] = []
+    for item in items:
+        result = predicate(item)
+        if result is not None:
+            compacted_items.append(result)
+
+    return compacted_items
--- 2024/Python/src/aoc/strings.py
+++
@@ -1,2 +0,0 @@
-def split_in_to_characters(string: str):
-    return list(string)
